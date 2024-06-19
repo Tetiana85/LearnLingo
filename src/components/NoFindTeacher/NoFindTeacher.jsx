b8381140a639b4e84492c48d254966de9327e3a6
TetianaL <4215654@gmail.com>
2024-06-19 22:29:03 +0300

    fixed img

--- src/components/NoFindTeacher/NoFindTeacher.jsx
+++ src/components/NoFindTeacher/NoFindTeacher.jsx
@@ -3,7 +3,7 @@ import styles from './NoFindTeacher.module.css';
 export const NoFindContainer = () => {
   return (
     <div className={styles.NoFindContainer}>
-      <img src="/src/assets/img/lupa.jpg" alt="No Find" />
+      <img src="./lupa.jpg" alt="No Find" />
       <p>Sorry, we didn't find any teachers for you!</p>
     </div>
   );
